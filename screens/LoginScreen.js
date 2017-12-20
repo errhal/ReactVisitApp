e5d363c6a351cc3451bee304aa80211088ad0f2c
errhal <errhal@gmail.com>
2017-12-20 01:18:18 +0100

    Added state param.

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -3,6 +3,13 @@ import { StyleSheet, Text, View, Button, WebView, AsyncStorage} from 'react-nati
 import { StackNavigator } from 'react-navigation';
 
 export class LoginScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      state: Math.random().toString(36)
+    };
+  }
+
   static navigationOptions = {
     title: 'Login',
     headerStyle: {backgroundColor: 'steelblue'},
@@ -16,10 +23,20 @@ export class LoginScreen extends React.Component {
         return;
       }
       let accessToken = null;
+      let state = null;
+
       for(var param in params) {
         if(params[param].match(/access_token=/g)) {
           accessToken = params[param].split("=")[1];
         }
+        if(params[param].match(/state=/g)) {
+          state = params[param].split("=")[1];
+        }
+      }
+
+      if (state != this.state.state) {
+        console.error("Invalid state param! Possible CSRF attack");
+        return;
       }
       if (accessToken != null) {
 
@@ -38,7 +55,7 @@ export class LoginScreen extends React.Component {
   render() {
     return (
       <WebView
-        source={{uri: 'https://zielonks.pythonanywhere.com/o/authorize?grant_type=implicit&client_id=JTgZ6X0SipUHnEy79brxjpbtsB734L6qLlPucqOX&redirect_uri=http://localhost:8000&response_type=token&state=1234567890'}}
+        source={{uri: 'https://zielonks.pythonanywhere.com/o/authorize?grant_type=implicit&client_id=JTgZ6X0SipUHnEy79brxjpbtsB734L6qLlPucqOX&redirect_uri=http://localhost:8000&response_type=token&state=' + this.state.state}}
         onNavigationStateChange={(e) => this.checkIfAuthorized(e)}
       />
     );
